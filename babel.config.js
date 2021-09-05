66f20ebd8cce947bec49810e68c49ecf7430bb78
saqqdy <saqqdy@qq.com>
2021-09-05 21:40:03 +0800

    改用rollup构建esm cmd包,umd包仍然用webpack构建

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,30 @@
 module.exports = {
-	presets: [
-		[
-			'@babel/preset-env',
-			{
-				useBuiltIns: 'entry',
-				corejs: 3,
-				targets: '> 0.5%, not dead'
-			}
-		]
-	]
+    presets: [
+        [
+            '@babel/env',
+            {
+                loose: true,
+                modules: 'auto',
+                useBuiltIns: 'usage',
+                corejs: 3
+                // exclude: ['es.promise', 'es.promise.finally']
+            }
+        ],
+        '@babel/typescript'
+    ],
+    plugins: [
+        [
+            '@babel/plugin-proposal-decorators',
+            {
+                legacy: true
+            }
+        ],
+        [
+            '@babel/plugin-proposal-class-properties',
+            {
+                loose: true
+            }
+        ]
+        // '@babel/plugin-transform-runtime'
+    ]
 }
